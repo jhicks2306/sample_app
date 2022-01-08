a2c9f7fe3dde2c47742a53fbab078ae2c102ad32
James Hicks <jhicks2306@gmail.com>
2022-01-07 17:09:56 -0800

    Add redirect for user save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # Not the final implentation!
     if @user.save
-      # Handle a succesful save.
+      redirect_to @user
     else
       render 'new'
     end
